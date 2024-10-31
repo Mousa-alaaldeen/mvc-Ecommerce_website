0895a7990f6d0ce63f13fbd255a0db443527d2ba
Mousa-alaaldeen <azmimousa396@gmail.com>
2024-11-01 00:45:43 +0300

    update header

--- views/partials/admin_header.php
+++ views/partials/admin_header.php
@@ -155,7 +155,6 @@
 									<div class="dropdown-menu-footer p-2 text-center">
 										<a href="notifications.html">View all</a>
 									</div>
-
 								</div><!--//dropdown-menu-->
 							</div><!--//app-utility-item-->
 							<div class="app-utility-item">
@@ -217,12 +216,11 @@
 							<!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
 							<a class="nav-link" href="/admin/manage_products">
 								<span class="nav-icon">
-									<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-folder"
+								<!-- <i class="fas fa-boxes"></i> -->
+								<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-columns-gap"
 										fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-										<path
-											d="M9.828 4a3 3 0 0 1-2.12-.879l-.83-.828A1 1 0 0 0 6.173 2H2.5a1 1 0 0 0-1 .981L1.546 4h-1L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3v1z" />
 										<path fill-rule="evenodd"
-											d="M13.81 4H2.19a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4zM2.19 3A2 2 0 0 0 .198 5.181l.637 7A2 2 0 0 0 2.826 14h10.348a2 2 0 0 0 1.991-1.819l.637-7A2 2 0 0 0 13.81 3H2.19z" />
+											d="M6 1H1v3h5V1zM1 0a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1H1zm14 12h-5v3h5v-3zm-5-1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1h-5zM6 8H1v7h5V8zM1 7a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H1zm14-6h-5v7h5V1zm-5-1a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1h-5z" />
 									</svg>
 								</span>
 								<span class="nav-link-text">Products</span>
@@ -233,12 +231,8 @@
 							<!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
 							<a class="nav-link" href="/admin/manage_customers">
 							<span class="nav-icon">
-										<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-file-person"
-											fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-											<path fill-rule="evenodd"
-												d="M12 1H4a1 1 0 0 0-1 1v10.755S4 11 8 11s5 1.755 5 1.755V2a1 1 0 0 0-1-1zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z" />
-											<path fill-rule="evenodd" d="M8 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-										</svg>
+							<i class="fas fa-users"></i>
+
 									</span>
 								<span class="nav-link-text">Customers</span>
 							</a><!--//nav-link-->
@@ -247,16 +241,8 @@
 							<!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
 							<a class="nav-link ac" href="/admin/manage_category">
 								<span class="nav-icon">
-									<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-card-list"
-										fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-										<path fill-rule="evenodd"
-											d="M14.5 3h-13a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
-										<path fill-rule="evenodd"
-											d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5z" />
-										<circle cx="3.5" cy="5.5" r=".5" />
-										<circle cx="3.5" cy="8" r=".5" />
-										<circle cx="3.5" cy="10.5" r=".5" />
-									</svg>
+								<i class="fas fa-tags"></i>
+
 								</span>
 
 								<span class="nav-link-text">category</span>
@@ -266,13 +252,8 @@
 							<!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
 							<a class="nav-link" href="/admin/manage_orders">
 								<span class="nav-icon">
-									<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-folder"
-										fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-										<path
-											d="M9.828 4a3 3 0 0 1-2.12-.879l-.83-.828A1 1 0 0 0 6.173 2H2.5a1 1 0 0 0-1 .981L1.546 4h-1L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3v1z" />
-										<path fill-rule="evenodd"
-											d="M13.81 4H2.19a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4zM2.19 3A2 2 0 0 0 .198 5.181l.637 7A2 2 0 0 0 2.826 14h10.348a2 2 0 0 0 1.991-1.819l.637-7A2 2 0 0 0 13.81 3H2.19z" />
-									</svg>
+								<i class="fas fa-receipt"></i>
+
 								</span>
 
 								<span class="nav-link-text">Orders</span>
@@ -400,4 +381,25 @@
 			</div><!--//sidepanel-inner-->
 		</div>
 	</header>
-	<script src="/public/js/header.js"></script>
+	<script >
+		document.addEventListener("DOMContentLoaded", function() {
+    // Get the current path
+    const currentPath = window.location.pathname;
+
+    // Select all nav-links
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    navLinks.forEach(link => {
+        // Check if the href attribute matches the current path
+        if (link.getAttribute('href') === currentPath) {
+            // Remove 'active' from any previously active link
+            document.querySelectorAll('.nav-link.active').forEach(activeLink => {
+                activeLink.classList.remove('active');
+            });
+
+            // Add 'active' class to the current link
+            link.classList.add('active');
+        }
+    });
+});
+	</script>
